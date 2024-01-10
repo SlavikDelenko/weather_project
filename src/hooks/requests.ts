3d24e9e5f12330405dcad7ac50d589c48ae8650a
fiyzech <svis5474@gmail.com>
2024-01-10 12:47:43 +0200

    added request to API

---
+++ src/hooks/requests.ts
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface WeatherData {
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+  }[];
+}
+
+interface ForecastData {
+  list: {
+    main: {
+      temp: number;
+    };
+    weather: {
+      description: string;
+    }[];
+  }[];
+}
+
+interface WeatherApiHook {
+  weatherData: WeatherData | null;
+  forecastData: ForecastData | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const useWeatherApi = (city: string): WeatherApiHook => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [forecastData, setForecastData] = useState<ForecastData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const apiKey = 'be062ae90cf3d3303f85298bf828843e';
+
+        // Current weather API URL
+        const currentWeatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        const currentWeatherResponse = await axios.get<WeatherData>(currentWeatherApiUrl);
+        setWeatherData(currentWeatherResponse.data);
+
+        // Forecast API URL (adjust `cnt` parameter to get more days of forecast)
+        const forecastApiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&cnt=3`;
+        const forecastResponse = await axios.get<ForecastData>(forecastApiUrl);
+        setForecastData(forecastResponse.data);
+
+        setLoading(false);
+        setError(null);
+      } catch (error) {
+        setLoading(false);
+        setError('Error fetching weather data');
+        setWeatherData(null);
+        setForecastData(null);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city]);
+
+  return { weatherData, forecastData, loading, error };
+};
+
+export default useWeatherApi;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import DisplayWeatherData from './components/Weather'; 
+import './index.css'; 
+ 
+ReactDOM.render( 
+  <React.StrictMode> 
+    <DisplayWeatherData city="Lviv" /> 
+  </React.StrictMode>, 
+  document.getElementById('root') 
+);
