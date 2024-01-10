93af780ba59f6b2f0e2a3beb123e94c052821bc9
Yaroslav Delenko <yarikdelenko111@gmail.com>
2024-01-10 12:34:52 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='w-50 bg-black'>
+        <h1 className='text-red-600'>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
