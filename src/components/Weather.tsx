88c00c9d0c2ebd78a506a618210a19af27850bad
Yaroslav Delenko <yarikdelenko111@gmail.com>
2024-01-10 13:11:06 +0200

    added backgraund video in file weaher

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Weather() {
   return (
-    <div>Weather</div>
+    <>
+        <div className="flex items-center justify-center h-screen relative">
+        <div className="w-full h-full absolute inset-0">
+          <video
+            className="w-full h-full object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src="https://cdn.pixabay.com/vimeo/284467863/clouds-17723.mp4?width=1280&hash=630d4b8671ccfbab803d700b604836277871f82a" />
+          </video>
+        </div>
+        
+        </div>
+    </>
   )
 }
