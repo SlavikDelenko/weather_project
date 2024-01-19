578c857e9edab17fa16dee7ddf3e1f50d1c7cc53
Yaroslav Delenko <yarikdelenko111@gmail.com>
2024-01-19 23:34:50 +0200

    made a feature to display videos for different weather

--- src/components/SearchCity.tsx
+++ src/components/SearchCity.tsx
@@ -1,16 +1,20 @@
-// SearchCity.tsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherDisplay from './WeekWeather';
 
-const SearchCity: React.FC = () => {
-  const [city, setCity] = useState<string>(''); // Initialize with an empty string
+interface SearchCityProps {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+}
 
-  const handleSearch = () => {
-    // Trigger the weather data fetch with the current city value
-    if (city.trim()) {
-      setCity(city.trim()); // Remove leading/trailing whitespaces and update the state
+const SearchCity: React.FC<SearchCityProps> = ({ setCity }) => {
+  const [cityInput, setCityInput] = useState<string>('');
+
+  useEffect(() => {
+
+    const trimmedCity = cityInput.trim();
+    if (trimmedCity) {
+      setCity(trimmedCity);
     }
-  };
+  }, [cityInput, setCity]);
 
   return (
     <>
@@ -20,8 +24,8 @@ const SearchCity: React.FC = () => {
             id="city"
             name="city"
             type="text"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
+            value={cityInput}
+            onChange={(e) => setCityInput(e.target.value)}
             className="border-b border-black py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit w-96"
             placeholder="Enter city name"
           />
@@ -29,19 +33,13 @@ const SearchCity: React.FC = () => {
             htmlFor="city"
             className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
           >
-            {/* ... Your SVG icon here ... */}
+
           </label>
         </div>
-        <button
-          onClick={handleSearch}
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md focus:outline-none"
-        >
-          Search
-        </button>
       </div>
 
-      {/* Pass the entered city to WeatherDisplay */}
-      {city && <WeatherDisplay city={city} />}
+
+      {cityInput && <WeatherDisplay city={cityInput} />}
     </>
   );
 };
